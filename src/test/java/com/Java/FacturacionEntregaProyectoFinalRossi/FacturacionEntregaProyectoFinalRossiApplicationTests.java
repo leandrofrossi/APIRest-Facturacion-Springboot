6a1a0a6e30201ec67f8b5f92d70b3c10495d18b0
Leandro Rossi <leanmatador@gmail.com>
2023-06-07 17:30:12 -0300

    inciando proyecto

---
+++ src/test/java/com/Java/FacturacionEntregaProyectoFinalRossi/FacturacionEntregaProyectoFinalRossiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java.FacturacionEntregaProyectoFinalRossi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionEntregaProyectoFinalRossiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
